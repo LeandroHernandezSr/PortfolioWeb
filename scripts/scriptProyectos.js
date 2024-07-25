4413af347ddd67da801851d4d4d9aba2277bf4ac
LeandroHernandezSr <l.hernandez071999@gmail.com>
2024-07-25 05:10:09 -0300

    Arreglos

--- scripts/scriptProyectos.js
+++ scripts/scriptProyectos.js
@@ -4,15 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
     proyectos.forEach(proyecto => {
         proyecto.addEventListener('mouseover', function() {
             const descripcion = proyecto.querySelector('.descripcion-proyecto');
-            const titulo=proyecto.querySelector('.titulo-proyecto');
-            titulo.style.display='block';
+            const titulo = proyecto.querySelector('.titulo-proyecto');
+            titulo.style.display = 'block';
             descripcion.style.display = 'block';
+            descripcion.style.animation = 'animacionAparicion 8s'; // Aplica la animación
         });
-        proyecto.addEventListener('mouseout',()=>{
+
+        proyecto.addEventListener('mouseout', () => {
             const descripcion = proyecto.querySelector('.descripcion-proyecto');
-            const titulo=proyecto.querySelector('.titulo-proyecto');
-            titulo.style.display='none';
+            const titulo = proyecto.querySelector('.titulo-proyecto');
+            titulo.style.display = 'none';
             descripcion.style.display = 'none';
-        })
+            descripcion.style.animation = ''; // Elimina la animación
+        });
     });
 });
